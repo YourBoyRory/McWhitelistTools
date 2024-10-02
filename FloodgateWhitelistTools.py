c05d81ef00fc494ef16e5bd056ea63f1ecd2654c
YourBoyRory <yourboyrory@gmail.com>
2024-10-01 22:47:32 -0400

    added tools for floodgate

---
+++ FloodgateWhitelistTools.py
@@ -0,0 +1,17 @@
+import requests
+from uuid import UUID
+
+class FloodgateWhitelistTools:
+
+    headers = {'x-api-key': None}
+
+    def __init__(self, API_key):
+        self.headers['x-api-key'] = API_key
+
+    def getXUID(self, username):
+        try:
+            response = requests.get(f"https://mcprofile.io/api/v1/bedrock/gamertag/{username}", headers=self.headers).json()
+            uuid = UUID(response['floodgateuid'])
+            return str(uuid)
+        except:
+            return None
